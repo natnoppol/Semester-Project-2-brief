cea812e633f01f5e0ba40bdf3d764da3c9ea634d
unknown <eightsbinary@gmail.com>
2024-12-05 23:47:02 +0700

    fix: carousel broken display

--- src/js/router/views/listing/index.js
+++ src/js/router/views/listing/index.js
@@ -1,34 +1,31 @@
-import { authGuard } from "../../../utilities/authGuard";
+import { authGuard } from '../../../utilities/authGuard';
 
-import controllers from "../../../controllers/index";
-import utils from "../../../utilities/utils";
+import controllers from '../../../controllers/index';
+import utils from '../../../utilities/utils';
 
-import Swiper from "swiper";
-import { Navigation, Controller } from "swiper/modules";
-import "swiper/css/bundle";
+import Swiper from 'swiper';
+import { Navigation, Controller } from 'swiper/modules';
+import 'swiper/css/bundle';
 
 async function init() {
-  const loadingIndicator = document.getElementById("loading-indicator");
-  loadingIndicator.classList.remove("hidden");
+  const loadingIndicator = document.getElementById('loading-indicator');
+  loadingIndicator.classList.remove('hidden');
 
   utils.humberger();
-  const container = document.querySelector(".container");
+  const container = document.querySelector('#listing-container');
   clearContent(container);
   try {
-    await authGuard();
-
-    const id = utils.getUrlParams("id");
+    const id = utils.getUrlParams('id');
     const listings = await fetchListings(id);
 
     renderListingsElement(listings, id, container);
-
     attachBidEvent(id);
   } catch (error) {
-    console.error("Error fetching Listings:", error);
+    console.error('Error fetching Listings:', error);
     container.innerHTML =
-      "<p>Error loading Listings. Please try again later.</p>";
+      '<p>Error loading Listings. Please try again later.</p>';
   } finally {
-    loadingIndicator.classList.add("hidden");
+    loadingIndicator.classList.add('hidden');
   }
 }
 
@@ -45,84 +42,83 @@ function renderListingsElement(listings, id, target) {
   attachDeleteEvent(id);
 
   // Initialize Swipers only after the elements are rendered
-  const slider = new Swiper(".gallery-slider", {
+  const slider = new Swiper('.gallery-slider', {
+    centeredSlides: true,
+    loop: true,
+    loopedSlides: 6,
+    modules: [Navigation, Controller],
     slidesPerView: 1,
-    centeredSlides: true,
-    loop: true,
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
     },
-    modules: [Navigation, Controller],
   });
-
-  const thumbs = new Swiper(".gallery-thumbs", {
-    slidesPerView: "auto",
-    spaceBetween: 10,
+  const thumbs = new Swiper('.gallery-thumbs', {
     centeredSlides: true,
     loop: true,
-    slideToClickedSlide: true,
     modules: [Navigation, Controller],
+    slidesPerView: 'auto',
+    slideToClickedSlide: true,
+    spaceBetween: 10,
   });
 
-  // Link the two Swipers after initialization
-  slider.controller.control = slider;
-  thumbs.controller.control = thumbs;
+  slider.controller.control = thumbs;
+  thumbs.controller.control = slider;
 }
 
 function clearContent(target) {
-  target.innerHTML = "";
+  target.innerHTML = '';
 }
 
-function swiperElem(listings) {
-  const listingElement = document.createElement("article");
-  listingElement.className = "flex md:flex-1 swiper-container gallery-slider";
+function swiperGalllery(listings) {
+  const listingElement = document.createElement('div');
+  listingElement.className = 'swiper-container gallery-slider overflow-hidden';
 
   const mediaSlides =
     listings.media
       ?.map(
         (mediaItem) => `
-      <div class="swiper-slide bg-white items-center">
-        <img class="w-full"
-          src="${mediaItem.url || ""}"
-          alt="${mediaItem.alt || "Media item"}" />
+      <div class="swiper-slide">
+        <img class="object-cover w-full max-h-[600px]"
+          src="${mediaItem.url || ''}"
+          alt="${mediaItem.alt || 'Media item'}" />
       </div>
   `
       )
-      .join("") || "";
+      .join('') || '';
 
   listingElement.innerHTML = `
-    <div class="swiper-wrapper z-10">
+    <div class="swiper-wrapper">
       ${mediaSlides}
     </div>
     
     <!-- If we need navigation buttons -->
-    <div class="swiper-button-prev z-20"></div>
-    <div class="swiper-button-next z-20"></div>
+    <div class="swiper-button-prev md:!top-[350px] lg:!left-[50px]"></div>
+    <div class="swiper-button-next md:!top-[350px] md:!right-[370px] lg:!right-[825px]"></div>
     
   `;
   return listingElement;
 }
 
-function swiperGallery(listings) {
-  const listingElement = document.createElement("article");
-  listingElement.className = "flex md:flex-1 swiper-container gallery-thumbs";
+function swiperThumbs(listings) {
+  const listingElement = document.createElement('div');
+  listingElement.className = 'swiper-container gallery-thumbs overflow-hidden';
 
   const mediaSlides =
     listings.media
       ?.map(
         (mediaItem) => `
-      <div class="swiper-slide bg-white items-center">
-        <img class="w-full"
-          src="${mediaItem.url || ""}"
-          alt="${mediaItem.alt || "Media item"}" />
+      <div class="swiper-slide bg-white items-center max-h-[100px] max-w-[100px]">
+        <img class="max-w-[100px] max-h-[100px]"
+          src="${mediaItem.url || ''}"
+          alt="${mediaItem.alt || 'Media item'}" />
       </div>
   `
       )
-      .join("") || "";
+      .join('') || '';
 
   listingElement.innerHTML = `
-    <div class="swiper-wrapper z-10">
+    <div class="swiper-wrapper max-h-[100px]">
       ${mediaSlides}
     </div>
   `;
@@ -130,8 +126,8 @@ function swiperGallery(listings) {
 }
 
 function renderListingInfoElem(listings) {
-  const listingInfoElem = document.createElement("div");
-  listingInfoElem.className = "p-4 max-w-full mx-auto break-all";
+  const listingInfoElem = document.createElement('div');
+  listingInfoElem.className = 'p-4 max-w-full mx-auto break-all';
   const listingCreated = utils.date(listings.created);
   const tags = utils.formatTags(listings.tags);
 
@@ -142,13 +138,17 @@ function renderListingInfoElem(listings) {
       </h2>
       <div class="flex flex-col">
         <div class="flex gap-4 relative ">
-          <a class="absolute top-1 left-1" href="/profile/?seller=${listings.seller.name}">
+          <a class="absolute top-1 left-1" href="/profile/?seller=${
+            listings.seller.name
+          }">
             <img class="rounded-full h-8 w-8" src="${
               listings.seller.avatar.url
             }" alt="${listings.seller.avatar.alt} width="32" height="32" />
           </a>
           <a href="/profile/?seller=${listings.seller.name}">
-            <h2 class="pl-10 leading-relaxed text-2xl font-bold text-black">${listings.seller.name}</h2>
+            <h2 class="pl-10 leading-relaxed text-2xl font-bold text-black">${
+              listings.seller.name
+            }</h2>
           </a>
         </div>
         
@@ -160,7 +160,9 @@ function renderListingInfoElem(listings) {
           ${tags}
         </div>  
         <div id="article-body" class="text-lg font-bold dark:text-red-700">
-          About this item: <span class="text-black">${listings.description}</span>
+          About this item: <span class="text-black">${
+            listings.description
+          }</span>
         </div>
         <form class="bid-form my-4" data-listing-id="LISTING_ID" id="bid" name="bid">
           <label for="bidAmount" class="leading-relaxed text-lg font-bold dark:text-red-700">Place Your Bid:</label>
@@ -188,7 +190,7 @@ function renderListingInfoElem(listings) {
           isSeller(listings.seller.name)
             ? `<button class="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800" id="editListing">Update</button>
           <button class="w-full text-white btn-danger-cancel bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800" id="deleteListing">Delete</button>`
-            : ""
+            : ''
         }
         </div>
         </ฝdivv>
@@ -199,35 +201,28 @@ function renderListingInfoElem(listings) {
   return listingInfoElem;
 }
 
-
-
 async function renderListings(listings, target) {
-  const container = document.createElement("div");
-  container.className = "gallery flex w-full sm:flex-col md:flex-row";
+  const mainContainer = document.querySelector('#listing-container');
 
   // Create the left column with Swiper
-  const swiperContainer = document.createElement("div");
-  swiperContainer.className = "w-full md:w-1/2";
-
-  const swiperElement = swiperElem(listings); // Get the Swiper DOM element
-  swiperContainer.appendChild(swiperElement); // Append the Swiper element
-
-  const galleryElement = swiperGallery(listings);
-  swiperContainer.appendChild(galleryElement);
+  const lCol = document.createElement('div');
+  lCol.id = 'l-col';
+  lCol.className = 'w-full md:w-1/2';
+  const swiperElement = swiperGalllery(listings);
+  lCol.appendChild(swiperElement);
+  const swiperThumbsElement = swiperThumbs(listings)
+  lCol.appendChild(swiperThumbsElement);
+  mainContainer.appendChild(lCol);
 
   // Create the right column with listing info
-  const listingInfo = document.createElement("div");
-  listingInfo.className = "w-full md:w-1/2";
-  const listingInfoElem = renderListingInfoElem(listings);
-  listingInfo.appendChild(listingInfoElem);
-  // listingInfo.textContent = "Listing Info"; // Replace with dynamic content as needed
+  const rCol = document.createElement('div');
+  rCol.id = 'r-col';
+  rCol.className = 'w-full md:w-1/2';
+  const listingInfo = renderListingInfoElem(listings);
+  rCol.appendChild(listingInfo);
+  mainContainer.appendChild(rCol);
 
-  // Append both columns to the second listing element
-  container.appendChild(swiperContainer);
-  container.appendChild(listingInfo);
-
-  // Append the second listing element to the target
-  target.appendChild(container);
+  target.appendChild(mainContainer);
 }
 
 function isSeller(seller) {
@@ -240,7 +235,7 @@ function attachBidEvent(id) {
   const form = document.forms.bid;
 
   if (form) {
-    form.addEventListener("submit", (event) => {
+    form.addEventListener('submit', (event) => {
       event.preventDefault();
       controllers.ListingsController.onBid(event, id);
     });
@@ -248,45 +243,45 @@ function attachBidEvent(id) {
 }
 
 function attachEditEvent(id) {
-  const editButton = document.getElementById("editListing");
+  const editButton = document.getElementById('editListing');
   if (editButton) {
-    editButton.addEventListener("click", () => {
+    editButton.addEventListener('click', () => {
       utils.redirectTo(`/listing/edit/?id=${id}`);
     });
   }
 }
 
 function attachDeleteEvent(id) {
-  const deleteButton = document.getElementById("deleteListing");
+  const deleteButton = document.getElementById('deleteListing');
   if (deleteButton) {
-    deleteButton.addEventListener("click", async () => {
+    deleteButton.addEventListener('click', async () => {
       const confirmed = window.confirm(
-        "Are you sure you want to delete this listing?"
+        'Are you sure you want to delete this listing?'
       );
       if (confirmed) {
         controllers.ListingsController.onDeleteListing(id);
       } else {
-        console.log("Delete action canceled");
+        console.log('Delete action canceled');
       }
     });
   }
 }
 
-function adjustContentHeight() {
-  const footer = document.querySelector("footer");
-  const header = document.querySelector("nav");
-  const main = document.querySelector(".container");
+// function adjustContentHeight() {
+//   const footer = document.querySelector("footer");
+//   const header = document.querySelector("nav");
+//   const main = document.querySelector(".container");
 
-  const viewportHeight = window.innerHeight;
-  const headerHeight = header.offsetHeight;
-  const footerHeight = footer.offsetHeight;
+//   const viewportHeight = window.innerHeight;
+//   const headerHeight = header.offsetHeight;
+//   const footerHeight = footer.offsetHeight;
 
-  // Set the main content height to fill the remaining space
-  main.style.minHeight = `${viewportHeight - headerHeight - footerHeight}px`;
-}
+//   // Set the main content height to fill the remaining space
+//   main.style.minHeight = `${viewportHeight - headerHeight - footerHeight}px`;
+// }
 
 // Adjust height on page load and resize
-window.addEventListener("load", adjustContentHeight);
-window.addEventListener("resize", adjustContentHeight);
+// window.addEventListener("load", adjustContentHeight);
+// window.addEventListener("resize", adjustContentHeight);
 
 init();
